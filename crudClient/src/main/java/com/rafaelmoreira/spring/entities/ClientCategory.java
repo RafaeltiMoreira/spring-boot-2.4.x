2568e6544e7f1cf860fe3e8bbe1f27f7ef4dabee
Rafael Moreira <rafaeltimoreirass@gmail.com>
2022-01-21 01:50:17 -0300

    Paginated search

--- crudClient/src/main/java/com/rafaelmoreira/spring/entities/ClientCategory.java
+++ crudClient/src/main/java/com/rafaelmoreira/spring/entities/ClientCategory.java
@@ -2,7 +2,6 @@ package com.rafaelmoreira.spring.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -24,6 +23,8 @@ public class ClientCategory implements Serializable {
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 	
@@ -113,9 +114,12 @@ public class ClientCategory implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -125,6 +129,11 @@ public class ClientCategory implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		ClientCategory other = (ClientCategory) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
--- crudClient/src/main/java/com/rafaelmoreira/spring/services/ClientService.java
+++ crudClient/src/main/java/com/rafaelmoreira/spring/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.rafaelmoreira.spring.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +25,10 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<ClientCategory> list = repository.findAll();
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<ClientCategory> list = repository.findAll(pageRequest);
 
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -75,5 +75,4 @@ public class ClientService {
 			throw new DatabaseException("Violation de integridade");
 		}
 	}
-
 }
